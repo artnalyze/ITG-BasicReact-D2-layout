9dddd5d91e370edee84a41b268abef11e3ed5be4
Artnalyze <artnalyze@gmail.com>
2021-12-05 22:33:18 +0700

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,11 @@
+import MainLayout from "../components/layouts/MainLayout"
+
 const About = () => {
     return (
-        <div>
-            
-        </div>
+        <MainLayout>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam minima nisi tempora quaerat ullam sapiente accusantium rerum, aperiam necessitatibus quasi enim consequatur, ad quam culpa illum laborum nemo delectus mollitia!</p>
+        </MainLayout>
     )
 }
 
