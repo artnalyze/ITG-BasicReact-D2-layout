a20ea3b95d31685112bff89b8f5a804e89880a55
Artnalyze <artnalyze@gmail.com>
2021-12-05 22:25:44 +0700

    update layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import Home from "./pages/Home";
+
 const App = () => {
   return (
-    <div className="">
-      <h1 className="bg-primary text-white">App Compnent</h1>
-    </div>
+    <Home />
   );
 }
 
